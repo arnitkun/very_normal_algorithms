0007ec5b2586bc07b7a9aaf291d2cc390284ab86
Ankit Arun <15148735+arnitkun@users.noreply.github.com>
2020-07-13 01:13:48 +0530

    Create prime_number_permutations.py
    python code to find all prime permutations of a number

---
+++ prime_number_permutations.py
@@ -0,0 +1,44 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+import itertools
+
+number = 935
+
+def digitize(n):
+    i = []
+    while(n):
+        i.append(n%10)
+        n //= 10
+    return i
+
+# print(digitize(number))
+
+perms = itertools.permutations(digitize(number))
+
+permlist = []
+for p in perms:
+    permlist.append(list(p))
+
+# print(permlist)
+
+places = len(permlist[0])
+places = 10**places
+# print(places)
+
+nums = []
+
+for p in permlist:
+   
+    # print(p)
+    
+    current_place = places
+    
+    n = 0
+    
+    for i in range(len(p)):
+        n += current_place*(p[i])
+        current_place = current_place//10
+    if n not in nums:
+            nums.append(n)
+print(nums)
+
